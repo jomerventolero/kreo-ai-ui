5741dd127a56af6e0d61b4990151ddd71bf3973e
rubszz <rubs76593@gmail.com>
2023-06-23 14:54:47 +0800

    Major Updates added animations

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,69 @@
-import Navbar from '@/components/Navbar'
-import Image from 'next/image'
+"use client";
+import { motion } from 'framer-motion';
+import Navbar from '@/components/Navbar';
+import Image from 'next/image';
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Navbar />
-      <Image src="/photo1.jpeg" className="absolute left-48 top-44 rounded-xl" width={180} height={260} />
-      <Image src="/photo4.jpeg" className="absolute right-48 top-44 rounded-xl" width={180} height={260} />
-      <Image src="/photo6.jpeg" className="absolute top-32 rounded-xl" width={180} height={260} />
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 1.1 }}
+        className="absolute left-48 top-48 rounded-xl z-10"
+      >
+        <h1 className="text-4xl font-bold glass-text text-left mb-8 leading-snug">
+          Generate <br/>Images from <br/>Text. 
+        </h1>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.2 }}
+        className="absolute right-1/2 top-48 rounded-xl"
+      >
+        <Image src="/photo1.jpeg" width={180} height={260} className='rounded-xl' />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
+        className="absolute right-96 top-44 rounded-xl"
+      >
+        <Image src="/photo4.jpeg" width={180} height={260} className='rounded-xl'/>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.6 }}
+        className="absolute top-32 left-1/2 rounded-xl"
+      >
+        <Image src="/photo6.jpeg" width={180} height={260} className='rounded-xl'/>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.8 }}
+        className="absolute top-96 right-98 rounded-xl"
+      >
+        <Image src="/photo7.jpeg" width={180} height={260} className='rounded-xl'/>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.9 }}
+        className="absolute top-80 left-96 rounded-xl"
+      >
+        <Image src="/photo8.jpg" width={180} height={260} className='rounded-xl'/>
+      </motion.div>
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="/generate"
+          href="https://3b2d-45-194-11-194.ngrok-free.app/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold transition-transform group-hover:translate-x-1 motion-reduce:transform-none`}>
-            {' '}
             GENERATE
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
@@ -26,5 +75,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
