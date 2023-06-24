d70b2cc2f7ec91a9508bdbc0c4ca37401618cfeb
orekihoutaro <orekihoutaro1218@gmail.com>
2023-06-24 13:20:42 +0800

    Generate UI

--- src/app/generate/page.jsx
+++ src/app/generate/page.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import Navbar from '@/components/Navbar'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <motion.div
+        className="px-10"
+        >
+        <Navbar />
+      </motion.div>
+      <motion.div
+        className=""
+        >
+        Enter Prompt Here:
+        <textarea id="prompt" defaultValue="studio ghibli, aesthetic, rendering, forest, swamp, scatter lighting, volumetrics, (masterpiece), best quality, (detailed)"/>
+      </motion.div>
+    </div>
   )
 }
 
