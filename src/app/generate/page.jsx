195b2064d0e21cb8d4ced3cb608cb28473dcd48d
orekihoutaro <orekihoutaro1218@gmail.com>
2023-06-27 09:53:46 +0800

    use client

--- src/app/generate/page.jsx
+++ src/app/generate/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, {useState} from 'react'
 import { motion } from 'framer-motion'
 import Navbar from '@/components/Navbar'
@@ -27,7 +27,7 @@ const page = () => {
   };
 
   return (
-    <div>
+    <div className='flex flex-col justify-center mx-auto'>
       <motion.div
         className="px-10 pt-4"
         >
@@ -35,12 +35,28 @@ const page = () => {
       </motion.div>
       <form>
         <motion.div
-          className="flex flex-col justify-start px-12 pt-4"
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.5 }}
+          className="flex flex-col justify-start px-12 pt-8"
           >
-          Enter Prompt Here:
+          <span className='font-sans pb-4'>Enter Prompt:</span>
           <textarea id="prompt"
             onChange={handleChange}
             className='p-2 w-1/4 h-[250px] rounded-xl text-black' defaultValue="studio ghibli, aesthetic, rendering, forest, swamp, scatter lighting, volumetrics, (masterpiece), best quality, (detailed)"/>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 1 }}
+          className="flex flex-col justify-start px-12 pt-8"
+          >
+          <span className='font-sans pb-4'>Negative Prompt:</span>
+          <textarea id="negative_prompt"
+            onChange={handleChange}
+            className='p-2 w-1/4 h-[100px] rounded-xl text-black' 
+            defaultValue="(easynegative),(signature:1.2),(artist name:1.2),(watermark:1.2)"
+          />
         </motion.div>
       </form>
     </div>
